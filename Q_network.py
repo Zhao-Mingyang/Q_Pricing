d6ed63b7aab4848515795f9f4585d814fcf7a163
Zhao Mingyang <67095541+Zhao-Mingyang@users.noreply.github.com>
2023-01-08 22:57:01 +0800

    Add files via upload

--- Q_network.py
+++ Q_network.py
@@ -4,18 +4,10 @@ import numpy as np
 from random import sample
 
 class QNetwork(nn.Module):
-    ''' The function approximation network for Estimator
-        It is just a series of tanh layers. All in/out are torch.tensor
-    '''
+
 
     def __init__(self, num_actions=2, state_shape=None, mlp_layers=None):
-        ''' Initialize the Q network
 
-        Args:
-            num_actions (int): number of legal actions
-            state_shape (list): shape of state tensor
-            mlp_layers (list): output size of each fc layer
-        '''
         super(QNetwork, self).__init__()
 
         self.num_actions = num_actions
@@ -34,11 +26,7 @@ class QNetwork(nn.Module):
         self.activation = nn.Softmax(dim=-1)
 
     def forward(self, s):
-        ''' Predict action values
 
-        Args:
-            s  (Tensor): (batch, state_shape)
-        '''
         # print(s.size())
         x = self.flatten(s)
         x = self.normalisation(x)
@@ -50,44 +38,21 @@ class QNetwork(nn.Module):
         return out
 
 class Memory(object):
-    ''' Memory for saving transitions
-    '''
 
     def __init__(self, memory_size, batch_size):
-        ''' Initialize
-        Args:
-            memory_size (int): the size of the memroy buffer
-        '''
         self.memory_size = memory_size
         self.batch_size = batch_size
         self.memory = []
 
     def save(self, state, price, action, reward):
-        ''' Save transition into memory
 
-        Args:
-            state (numpy.array): the current state
-            action (int): the performed action ID
-            reward (float): the reward received
-            next_state (numpy.array): the next state after performing the action
-            legal_actions (list): the legal actions of the next state
-            done (boolean): whether the episode is finished
-        '''
         if len(self.memory) == self.memory_size:
             self.memory.pop(0)
         transition = [state, price, action, reward]
         self.memory.append(transition)
 
     def pop_batch(self):
-        ''' Sample a minibatch from the replay memory
 
-        Returns:
-            state_batch (list): a batch of states
-            action_batch (list): a batch of actions
-            reward_batch (list): a batch of rewards
-            next_state_batch (list): a batch of states
-            done_batch (list): a batch of dones
-        '''
         samples = sample(self.memory, self.batch_size)
         # samples = self.memory[-self.batch_size:]
         # print(len(samples))
